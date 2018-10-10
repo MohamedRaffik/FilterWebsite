24c030574fd26db6ee99981e6f081d79c270aa73
Hasan Abdullah <37593075+HasanAbdullah31@users.noreply.github.com>
2018-10-09 23:41:33 -0400

    Finish javascript for download button and change CSS

--- app/static/logic.js
+++ app/static/logic.js
@@ -14,15 +14,23 @@ function upload(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function(load_event) {
-            $('#old-img').attr('src', load_event.target.result);
+            var content = load_event.target.result;
+            $('#old-img').attr('src', content);
+            $('#new-img').attr('src', content);
         };
+        // readAsDataURL represents the image as a base64 encoded string that
+        // starts with the regexp 'data:*/*;base64,' so may need to remove:
+        // var search_value = new RegExp('data:*/*;base64,');
+        // console.log( content.substring(content.indexOf(',')+1) );
         reader.readAsDataURL(input.files[0]);
-        //var old_img = document.getElementById('old-img');
-        //old_img.height = 300;
-        //old_img.width = 300;
     }
 }
 
 function download() {
-    ;
+    var link = document.createElement('a');
+    link.setAttribute('href', document.getElementById('new-img').src);
+    link.setAttribute('download', 'filterx-download.jpg');
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
 }
