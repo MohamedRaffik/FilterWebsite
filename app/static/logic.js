79976d8cb48e349d7a4284130f6866f0362adbf1
Hasan Abdullah <37593075+HasanAbdullah31@users.noreply.github.com>
2018-11-06 15:31:00 -0500

    Fix url upload

--- app/static/logic.js
+++ app/static/logic.js
@@ -1,4 +1,5 @@
 var filter_buttons_active = true;
+var upload_active = true;
 
 function change_theme() {
     if (document.body.className == 'dark') {
@@ -11,16 +12,16 @@ function change_theme() {
 
 function filter(filter_type) {
     if (!filter_buttons_active)
-        alert('Wait for current filter to finish processing!');
+        alert('Wait for current upload or filter to finish processing!');
     else if (document.getElementById('old-img').className == 'default')
         alert('Upload an image before clicking a filter button!');
     else if (document.getElementById('old-img').src.indexOf('.') != -1)
         alert('Cannot filter image! Try another upload method.');
     else {
-        // Prevent multiple filter() calls while processing a filter
-        filter_buttons_active = false;
+        // Prevent upload as well as filter() calls while processing a filter
+        filter_buttons_active = false; upload_active = false;
         document.getElementById('socials').className = 'hide';
-        document.getElementById('loading').className = 'show';
+        document.getElementById('filter-loading').className = 'show';
         var wait_song = new Audio('../static/sounds/runescape_harmony.mp3');
         wait_song.volume = 0.25; wait_song.play(); wait_song.loop = true;
 
@@ -33,9 +34,9 @@ function filter(filter_type) {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 wait_song.pause();
                 document.getElementById('new-img').src = xhr.responseText;
-                document.getElementById('loading').className = 'hide';
+                document.getElementById('filter-loading').className = 'hide';
                 document.getElementById('socials').className = 'show';
-                filter_buttons_active = true;
+                filter_buttons_active = true; upload_active = true;
             }
         };
         // Add time to URL to keep AJAX call unique and not cached by browser
@@ -44,67 +45,113 @@ function filter(filter_type) {
     }
 }
 
+function is_valid_b64_img(str) {
+    return str.search('data:image/.*;base64,') != -1;
+}
+
+function is_valid_url(url) {
+    var a = document.createElement('a'); a.href = url;
+    return a.host && a.host != window.location.host;
+}
+
 function update_images(b64_string) {
-    var click_sound = new Audio('../static/sounds/camera_sound.mp3');
-    click_sound.volume = 0.5; click_sound.play();
-    document.getElementById('drop-area').className = 'hide';
-    document.getElementById('old-img').src = b64_string;
-    document.getElementById('old-img').className = 'not-default';
-    document.getElementById('new-img').src = b64_string;
-    document.getElementById('new-img').className = 'not-default';
+    if (!is_valid_b64_img(b64_string))
+        alert('Unable to upload item! Try another upload method.');
+    else {
+        var click_sound = new Audio('../static/sounds/camera_sound.mp3');
+        click_sound.volume = 0.5; click_sound.play();
+        document.getElementById('drop-area').className = 'hide';
+        document.getElementById('old-img').src = b64_string;
+        document.getElementById('old-img').className = 'not-default';
+        document.getElementById('new-img').src = b64_string;
+        document.getElementById('new-img').className = 'not-default';
+    }
 }
 
 function url_upload(url) {
-    var pattern = new RegExp('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$');
-    if (!pattern.test(url))
-        alert('Invalid image URL! Try again.');
+    if (!upload_active)
+        alert('Wait for current upload or filter to finish processing!');
     else {
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/urlInput', true);
-        xhr.setRequestHeader('content-type',
-                             ' application/x-www-form-urlencoded; charset=UTF-8');
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                update_images(xhr.responseText);
+        var start_pos = url.indexOf('imgurl=');
+        if (start_pos != -1) {
+            start_pos += 7;
+            var end_pos = url.indexOf('&', start_pos);
+            if (end_pos != -1) {
+                // Read after 'imgurl=' and before '&' (& starts next URL param)
+                url = decodeURIComponent( url.substring(start_pos, end_pos) );
             }
-        };
-        xhr.send('img_url=' + url + '&t=' + new Date().getTime());
+        }
+
+        if (!is_valid_url(url))
+            alert('Invalid URL! Try again.');
+        else {
+            // Prevent upload as well as filter() calls while processing an upload
+            filter_buttons_active = false; upload_active = false;
+            document.getElementById('upload-options').className = 'hide';
+            document.getElementById('upload-loading').className = 'show';
+            var proxy_url = 'https://cors-anywhere.herokuapp.com/';
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', proxy_url + url, true);
+            xhr.responseType = 'blob';
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    var reader = new FileReader();
+                    reader.onload = function() {
+                        update_images(reader.result);
+                        document.getElementById('upload-loading').className = 'hide';
+                        document.getElementById('upload-options').className = 'show';
+                        filter_buttons_active = true; upload_active = true;
+                    };
+                    reader.readAsDataURL(xhr.response);
+                }
+            };
+            xhr.send();
+        }
     }
 }
 
 function upload(input) {
-    if (!input)
-        alert('Unable to upload item! Try another upload method.');
-    else if (input.files && input.files[0]) {
-        // input is a FileList object obtained from file item (upload or drop)
-        if (input.files[0].type.indexOf('image') == -1)
-            alert('Only image files can be uploaded! Try again.');
-        else {
-            var reader = new FileReader();
-            reader.onload = function(event) {
-                update_images(event.target.result);
-            };
-            // readAsDataURL represents the image as a base64 encoded string
-            // that starts with the regexp 'data:*/*;base64,'
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
+    if (!upload_active)
+        alert('Wait for current upload or filter to finish processing!');
     else {
-        // input is a DataTransfer object obtained from remote item (drop)
-        var html_string = input.getData('text/html');
-        // html_string has the following substring: src="src_string"
-        // where src_string is possibly a base64 encoded string
-        if (!html_string || html_string.indexOf('src="') == -1)
-            alert('Dropped item is not a valid image! Try again.');
-        else {
-            var start_pos = html_string.indexOf('src="')+5;
-            var src_string = html_string.substring(
-                start_pos, html_string.indexOf('"', start_pos)
-            );
-            if (src_string.indexOf(';base64,') == -1)
-                alert('Unable to upload image! Try another upload method.');
+        if (!input)
+            alert('Unable to upload item! Try another upload method.');
+        else if (input.files && input.files[0]) {
+            // input is a FileList object obtained from file item (upload or drop)
+            if (input.files[0].type.indexOf('image') == -1)
+                alert('Only image files can be uploaded! Try again.');
             else {
-                update_images(src_string);
+                var reader = new FileReader();
+                reader.onload = function() {
+                    update_images(reader.result);
+                };
+                // readAsDataURL represents the image as a base64 encoded string
+                // that starts with the regexp 'data:*/*;base64,'
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+        else {
+            // input is a DataTransfer object obtained from remote item (drop)
+            var text_string = input.getData('text');
+            var html_string = input.getData('text/html');
+            var start_pos = html_string.indexOf('src="');
+            if (html_string && start_pos != -1) {
+                start_pos += 5;
+                var end_pos = html_string.indexOf('"', start_pos);
+                if (end_pos != -1) {
+                    var src_string = html_string.substring(start_pos, end_pos);
+                    text_string = new DOMParser().parseFromString(
+                        src_string, 'text/html').body.textContent;
+                }
+            }
+
+            if (is_valid_b64_img(text_string)) {
+                update_images(text_string);
+            }
+            else if (!text_string || !is_valid_url(text_string))
+                alert('Dropped item is not a valid image! Try again.');
+            else {
+                url_upload(text_string);
             }
         }
     }
@@ -139,4 +186,4 @@ window.onload = function() {
     ['dragleave', 'drop'].forEach(function(event) {
         drop_area.addEventListener(event, unhighlight, false);
     });
-}
+};
