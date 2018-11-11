714bc8f1279fd514cc0d50833bedde32d6e2a1c5
Hasan Abdullah <37593075+HasanAbdullah31@users.noreply.github.com>
2018-11-11 12:36:47 -0500

    Change fb button

--- app/static/socials.js
+++ app/static/socials.js
@@ -1,16 +1,13 @@
-function upload_to_cloudinary(b64_string) {
+// Uploads b64_string as an image to Cloudinary, and then invokes callback
+// with the argument as the url of the uploaded image
+function upload_to_cloudinary(b64_string, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://api.cloudinary.com/v1_1/filterx/upload', true);
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var response = JSON.parse(xhr.responseText);
-            console.log('Secure URL: ' + response.secure_url);
-            console.log('Public ID: ' + response.public_id);
-            // Create a thumbnail of the uploaded image, with 150px width
-            var tokens = response.secure_url.split('/');
-            tokens.splice(-2, 0, 'w_150,c_scale');
-            console.log('Thumbnail URL: ' + tokens.join('/'));
+            callback(response.secure_url);
         }
     };
     var form_data = new FormData();
@@ -20,18 +17,27 @@ function upload_to_cloudinary(b64_string) {
     xhr.send(form_data);
 }
 
-function share(to_website) {
+function share_to(website) {
     if (document.getElementById('new-img').className == 'default') {
         alert('Upload an image before clicking the share button!');
         return false;
     }
-    upload_to_cloudinary(document.getElementById('new-img').src);
-    if (to_website == 'facebook') {
-        console.log('Facebook Share');
+    var callback;
+    if (website == 'facebook') {
+        callback = function(url) {
+            var link = document.createElement('a');
+            link.href = 'https://www.facebook.com/sharer/sharer.php?u=' +
+                encodeURIComponent(url);
+            link.target = '_blank';
+            link.click();
+        };
     }
-    else if (to_website == 'twitter') {
-        console.log('Twitter Share');
+    else if (website == 'twitter') {
+        callback = function(url) {
+            console.log('Twitter Share: ' + url);
+        };
     }
+    upload_to_cloudinary(document.getElementById('new-img').src, callback);
     // Allow anchor tag's href to follow through after function call
     return true;
 }
