94879ffc15cc9898ecdee41cc66b50f196568cf9
chuckolate <chukho.wu@gmail.com>
2018-12-19 16:16:29 -0500

    remove meme quote since it does not register

--- app/filter.py
+++ app/filter.py
@@ -181,7 +181,6 @@ def filter(b64_img, filter_type):
                  "Life Saving Tip: Do not eat erasers. Even if they smell like Japanese blue soda candy!",\
                  "YOU'RE*",\
                  "Can you get in done by tonight?",\
-                 "I love our logo (ノಠ益ಠ)ノ彡┻━┻",\
                  "I red dictionaries durig my childhood, thas why i splle goo",\
                  "I love deadlines. I love the whooshing noise they make as they go by."
         quote = random.choice(quotes)
